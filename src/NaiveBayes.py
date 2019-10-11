58ced020a753bea20762fb1aee2985888a77402e
Thomas Racine <thomas.racine98@gmail.com>
2019-10-11 10:06:09 -0400

    set up basic project structure, added directories for code and data, put the datasets inside the data directory

---
+++ src/NaiveBayes.py
@@ -0,0 +1,7 @@
+###########################################################
+# File name: NaiveBayes.py
+# Author: Thomas Racine
+# Purpose: implement the Naive Bayes classification model
+###########################################################
+
+import numpy as np
