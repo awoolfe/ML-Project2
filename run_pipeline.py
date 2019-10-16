024eb3d223f0dd92701187221dd4230274fbc60d
Alessia Woolfe <alessia.woolfe11@gmail.com>
2019-10-16 11:26:31 -0400

    scikit learn linear svm testing (sgdc clf)

--- run_pipeline.py
+++ run_pipeline.py
@@ -6,6 +6,13 @@ from itertools import chain
 from src.pipeline_utils import stratified_k_folds, evaluate_acc, ngram_to, ngram_tokenize
 from src.model.BernoulliNaiveBayes import BernoulliNaiveBayes
 from sklearn.naive_bayes import BernoulliNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 
 if __name__ == '__main__':
     K = 5
@@ -25,25 +32,43 @@ if __name__ == '__main__':
     vocab_itos = [OOV_TOKEN] + [word for word, count in vocab.most_common(VOCAB_SIZE - 1)]
     vocab_stoi = {k:i for i,k in enumerate(vocab_itos)}
 
+    '''TESTING SKLEARN PIPELINE AND SVM'''
+    sgd = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', SGDClassifier(max_iter=10))])
+
     train_folds, valid_folds = stratified_k_folds(df, Y_COL, K)
     for i, (train, valid) in enumerate(zip(train_folds, valid_folds)):
+        #sklearn implementation linear support vector machines
+        X_train1, X_test1, y_train1, y_test1 = train_test_split(train[X_COL].to_list(), train[Y_COL].to_list(), test_size=0.3, random_state=42)
+
         X_train = [ngram_to(x, vocab_stoi, NGRAM) for x in train[X_COL].to_list()]
         X_valid = [ngram_to(x, vocab_stoi, NGRAM) for x in valid[X_COL].to_list()]
         Y_train = train[Y_COL].to_list()
         Y_valid = valid[Y_COL].to_list()
 
         model = BernoulliNaiveBayes()
-        model2 = BernoulliNB()
+        #model2 = BernoulliNB()
+        dtree = DecisionTreeClassifier(criterion="gini", splitter="best", max_depth=3)
+        kneighbor = KNeighborsClassifier(n_neighbors=5)
 
-        model.fit(np.array(X_train), np.array(Y_train))
-        model2.fit(np.array(X_train), np.array(Y_train))
+        sgd.fit(X_train1, y_train1)
 
-        valid_pred = model.predict(np.array(X_valid))
-        valid_pred2 = model2.predict(np.array(X_valid))
-        print(valid_pred)
+        #model.fit(np.array(X_train), np.array(Y_train))
+        #model2.fit(np.array(X_train), np.array(Y_train))
+        #dtree.fit(np.array(X_train), np.array(Y_train))
+        #kneighbor.fit(np.array(X_train), np.array(Y_train))
+
+        #valid_pred = model.predict(np.array(X_valid))
+        #valid_pred2 = model2.predict(np.array(X_valid))
+        #dtree_pred = dtree.predict(np.array(X_valid))
+        #kneighbor_pred = kneighbor.predict(np.array(X_valid))
+        sgd_pred = sgd.predict(X_test1)
+        #print(valid_pred)
         print(np.array(Y_valid))
-        print(evaluate_acc(valid_pred, Y_valid))
-        print(evaluate_acc(valid_pred2, Y_valid))
+        #print(evaluate_acc(valid_pred, Y_valid))
+        #print(evaluate_acc(valid_pred2, Y_valid))
+        #print(evaluate_acc(kneighbor_pred, Y_valid))
+        #print(evaluate_acc(dtree_pred, Y_valid))
+        print(evaluate_acc(sgd_pred, y_test1))
         print("-"*80)
 
 
