69e43cb6f862c44fb45fad1b87517ee8a5b25de0
Thomas Racine <thomas.racine98@gmail.com>
2019-10-17 20:31:33 -0400

    fixed predict from ensemble

--- src/model/stackingEnsemble.py
+++ src/model/stackingEnsemble.py
@@ -5,21 +5,24 @@ class stackingEnsemble:
     def __init__(self, modelList):
         self.models = modelList
         self.metaModel = LogisticRegression()
+        self.classes = None
 
     def fit(self, X, y):
-        classes = np.unique(y)
+        self.classes = np.unique(y)
         for model in self.models:
             model.fit(X, y)
         predictions = np.empty((X.shape[0], len(self.models)), dtype='object')
         for i in range(len(self.models)):
             model_predictions = self.models[i].predict(X)
-            model_predictions = np.array([np.where(classes == x)[0] for x in model_predictions])
+            model_predictions = np.array([np.where(self.classes == x)[0] for x in model_predictions])
             predictions[:,i] = model_predictions[:,0]
         self.metaModel.fit(predictions, y)
 
 
     def predict(self, X):
-        predictions = np.array(([]), dtype=object)
-        for model in self.models:
-            predictions = np.append(predictions, model.predict(X))
+        predictions = np.empty((X.shape[0], len(self.models)), dtype='object')
+        for i in range(len(self.models)):
+            model_predictions = self.models[i].predict(X)
+            model_predictions = np.array([np.where(self.classes == x)[0] for x in model_predictions])
+            predictions[:, i] = model_predictions[:, 0]
         return self.metaModel.predict(predictions)
