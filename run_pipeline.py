d6d992f843e8625de1a205b32ba503a152de55ee
Thomas Racine <thomas.racine98@gmail.com>
2019-10-20 08:44:20 -0400

    bow keras best accuracy setting 54.84%

--- run_pipeline.py
+++ run_pipeline.py
@@ -12,16 +12,17 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
 from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
 
 if __name__ == '__main__':
     K = 2
-    VOCAB_SIZE = 1_000
+    VOCAB_SIZE = 10_000
     OOV_TOKEN = "--UNK--"
     NGRAMS = [1,2]
     Y_COL = "subreddits"
     X_COL = "preprocessed_comments"
     MODELS = [DecisionTreeClassifier(), KNeighborsClassifier(), LogisticRegression(), SGDClassifier(), MultinomialNB()]
-    PCA_N = 100
+    PCA_N = 1000
 
     print("reading data, dropping na's and creating cross-validation folds.")
     df = pd.read_csv("data/reddit_spacy_train.csv")
@@ -64,40 +65,40 @@ if __name__ == '__main__':
 
         print("fitting PCA to training set and transforming data...")
         pca = PCA(PCA_N)
-        X_train = pca.fit_transform(X_train)
-        X_valid = pca.transform(X_valid)
-        
-        print("converting PCA matrix to binary features...")
-        # TODO: this doesn't seem right?
-        X_train[X_train >= 0] = 1
-        X_train[X_train < 0] = 0
-        X_valid[X_valid >= 0] = 1
-        X_valid[X_valid < 0] = 0
+        X_trainPCA = pca.fit_transform(X_train)
+        X_validPCA = pca.transform(X_valid)
+
+        # print("converting PCA matrix to binary features...")
+        # # TODO: this doesn't seem right?
+        # X_train[X_train >= 0] = 1
+        # X_train[X_train < 0] = 0
+        # X_valid[X_valid >= 0] = 1
+        # X_valid[X_valid < 0] = 0
         
 
-        print("Instantiating model...")
-        model = BernoulliNaiveBayes()
-        model2 = MultinomialNB()
-        model3 = stackingEnsemble(MODELS)
+        #print("Instantiating model...")
+        model = LogisticRegression()
+        #model2 = MultinomialNB()
+        #model3 = stackingEnsemble(MODELS)
 
         print("Fitting model...")
-        model.fit(np.array(X_train), np.array(Y_train))
-        model2.fit(np.array(X_train), np.array(Y_train))
-        model3.fit(np.array(X_train), np.array(Y_train))
+        #model.fit(np.array(X_train), np.array(Y_train))
+        model.fit(np.array(X_trainPCA), np.array(Y_train))
+        #model3.fit(np.array(X_train), np.array(Y_train))
 
         print("Evaluating model...")
-        valid_pred = model.predict(np.array(X_valid))
+        #valid_pred = model.predict(np.array(X_valid))
 
-        valid_pred2 = model2.predict(np.array(X_valid))
-        train_pred2 = model2.predict(np.array(X_train))
-        valid_pred3 = model3.predict(np.array(X_valid))
+        valid_pred2 = model.predict(np.array(X_validPCA))
+        #train_pred2 = model2.predict(np.array(X_train))
+        #valid_pred3 = model3.predict(np.array(X_valid))
 
         print("Scikit Learn:")
         print(evaluate_acc(valid_pred2, Y_valid))
-        print("OUr model")
-        print(evaluate_acc(valid_pred, Y_valid))
-        print("ensemble Model")
-        print(evaluate_acc(valid_pred3, Y_valid))
+        #print("OUr model")
+        #print(evaluate_acc(valid_pred, Y_valid))
+        #print("ensemble Model")
+        #print(evaluate_acc(valid_pred3, Y_valid))
         # print(evaluate(train_pred2, Y_train, label_stoi))
         # print(evaluate(valid_pred, Y_valid, label_stoi))
         print("-"*80)
---
+++ src/bow_keras.py
@@ -0,0 +1,111 @@
+####################################################################
+# Taken from https://www.kdnuggets.com/2018/11/multi-class-text-classification-model-comparison-selection.html
+
+######################################################################
+
+
+import itertools
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import re
+from bs4 import BeautifulSoup
+import nltk
+from nltk.corpus import stopwords
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+from sklearn.metrics import confusion_matrix
+
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.preprocessing import text, sequence
+from keras import utils
+
+df = pd.read_csv('../data/reddit_train.csv')
+df2 = pd.read_csv('../data/reddit_test.csv')
+
+####################################################################
+# Text Preprocessing                                               #
+####################################################################
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+STOPWORDS = set(stopwords.words('english'))
+
+
+def clean_text(text):
+    """
+        text: a string
+
+        return: modified initial string
+    """
+    text = BeautifulSoup(text, "lxml").text  # HTML decoding
+    text = text.lower()  # lowercase text
+    text = REPLACE_BY_SPACE_RE.sub(' ', text)  # replace REPLACE_BY_SPACE_RE symbols by space in text
+    text = BAD_SYMBOLS_RE.sub('', text)  # delete symbols which are in BAD_SYMBOLS_RE from text
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS)  # delete stopwors from text
+    return text
+
+
+df['comments'] = df['comments'].apply(clean_text)
+df2['comments'] = df2['comments'].apply(clean_text)
+
+
+
+
+train_size = int(len(df) * 0.8)
+train_posts = df['comments'][:train_size]
+train_tags = df['subreddits'][:train_size]
+
+test_posts = df['comments'][train_size:]
+test_tags = df['subreddits'][train_size:]
+
+max_words = 25_000
+tokenize = text.Tokenizer(num_words=max_words, char_level=False)
+tokenize.fit_on_texts(train_posts)  # only fit on train
+
+x_train = tokenize.texts_to_matrix(train_posts)
+x_test = tokenize.texts_to_matrix(test_posts)
+
+encoder = LabelEncoder()
+encoder.fit(train_tags)
+y_train = encoder.transform(train_tags)
+y_test = encoder.transform(test_tags)
+
+num_classes = np.max(y_train) + 1
+y_train = utils.to_categorical(y_train, num_classes)
+y_test = utils.to_categorical(y_test, num_classes)
+
+batch_size = 100
+epochs = 1
+
+# Build the model
+model = Sequential()
+model.add(Dense(512, input_shape=(max_words,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_split=0.1)
+
+df2['comments'] = encoder.classes_[model.predict_classes(x_test, batch_size=batch_size, verbose=1)]
+
+df2.columns = ['Id', 'Category']
+
+df2['Category'].to_csv("../data/predictions.csv")
+
+#score = model.evaluate(x_test, y_test,
+#                       batch_size=batch_size, verbose=1)
+#print('Test accuracy:', score[1])
+
